29fb445be21456c2b2a25594c76fca347622ba43
João Gonçalves <joao.goncalves@pyyne.com>
2024-06-19 20:56:21 -0300

    feat: add repo method to retrieve last 30 day logs

--- sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/repositories/SleepLogRepository.kt
+++ sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/repositories/SleepLogRepository.kt
@@ -9,4 +9,7 @@ import java.time.LocalDate
 @Repository
 interface SleepLogRepository : JpaRepository<SleepLog, Long> {
     fun findByUserIdAndDate(userId: Long, date: LocalDate): SleepLog?
+
+    @Query("SELECT s FROM SleepLog s WHERE s.userId = :userId AND s.date >= :startDate")
+    fun findLast30DaysLogs(userId: Long, startDate: LocalDate): List<SleepLog>
 }
