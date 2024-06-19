a825c2549d9db48295525462a41c1964b2401a16
João Gonçalves <joao.goncalves@pyyne.com>
2024-06-19 19:55:39 -0300

    feat: add SleepLog entity

---
+++ sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/entities/SleepLog.kt
@@ -0,0 +1,24 @@
+package com.noom.interview.fullstack.sleep.entities
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import java.time.LocalDate
+import java.time.LocalTime
+import javax.persistence.*
+
+@Entity
+@Table(
+    uniqueConstraints = [UniqueConstraint(columnNames = ["userId", "date"])]
+)
+data class SleepLog(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val userId: Long = 0,
+    val date: LocalDate = LocalDate.now(),
+    val timeInBedStart: LocalTime = LocalTime.MIDNIGHT,
+    val timeInBedEnd: LocalTime = LocalTime.MIDNIGHT,
+    val totalTimeInBed: Long = 0L,
+    val morningFeeling: String = ""
+) {
+    // No-arg constructor for JPA
+    constructor() : this(0, 0, LocalDate.now(), LocalTime.MIDNIGHT, LocalTime.MIDNIGHT, 0L, "")
+}
