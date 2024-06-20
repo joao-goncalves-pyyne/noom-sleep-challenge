18c4f5e97268714d8e1a61a51b1d804c9548ce41
João Gonçalves <joao.goncalves@pyyne.com>
2024-06-19 21:10:12 -0300

    feat: add endpoint and postman request

--- sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/controllers/SleepLogController.kt
+++ sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/controllers/SleepLogController.kt
@@ -1,5 +1,6 @@
 package com.noom.interview.fullstack.sleep.controllers
 
+import com.noom.interview.fullstack.sleep.dtos.AverageSleepStats
 import com.noom.interview.fullstack.sleep.entities.SleepLog
 import com.noom.interview.fullstack.sleep.services.SleepLogService
 import org.springframework.http.ResponseEntity
@@ -17,5 +18,10 @@ class SleepLogController(private val sleepLogService: SleepLogService) {
     @GetMapping("/last-night")
     fun getLastNightSleep(@RequestParam userId: Long): ResponseEntity<SleepLog?> {
         return ResponseEntity.ok(sleepLogService.getLastNightSleep(userId))
+    }
+
+    @GetMapping("/30-day-averages")
+    fun get30DayAverages(@RequestParam userId: Long): ResponseEntity<AverageSleepStats> {
+        return ResponseEntity.ok(sleepLogService.get30DayAverages(userId))
     }
 }
